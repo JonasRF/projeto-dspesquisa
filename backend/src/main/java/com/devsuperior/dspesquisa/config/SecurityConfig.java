71b240bba3b15007c4ffa442a76fc8e1832c7203
Jonas Ribeiro <jonasribeiro41@yahoo.com.br>
2022-07-08 14:23:42 -0300

    Update SecurityConfig

--- backend/src/main/java/com/devsuperior/dspesquisa/config/SecurityConfig.java
+++ backend/src/main/java/com/devsuperior/dspesquisa/config/SecurityConfig.java
@@ -1,4 +1,4 @@
-package com.devsuperior.dspesquisa.condig;
+package com.devsuperior.dspesquisa.config;
 
 import java.util.Arrays;
 
@@ -8,24 +8,23 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-@SuppressWarnings("deprecation")
 @Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter {
-
+public class SecurityConfig {
+	
 	@Autowired
 	private Environment env;
 
 	private static final String[] PUBLIC_MATCHERS = { "/**" };
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
 		// H2
 		if (Arrays.asList(env.getActiveProfiles()).contains("test")) {
@@ -35,6 +34,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		http.cors().and().csrf().disable();
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 		http.authorizeRequests().antMatchers(PUBLIC_MATCHERS).permitAll();
+		return http.build();
 	}
 
 	@Bean
