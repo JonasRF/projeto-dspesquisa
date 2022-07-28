fd1b9c7b4d741f79883bfd6efbcd65c393dc1f44
Jonas Ribeiro <jonasribeiro41@yahoo.com.br>
2022-07-27 21:32:50 -0300

    Page Home

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,15 @@
 import './App.css'
 import Header from './Components/Header'
+import Home from './Pages/Home'
 
 function App() {
 
   return (
     <div>
       <Header />
+      <Home />
     </div>
-  )
+  );
 }
 
 export default App
