bc91a7147882f5f6de21d7a22f569172df33c7bf
Jonas Ribeiro <jonasribeiro41@yahoo.com.br>
2022-07-29 22:14:01 -0300

    layout finish

--- frontend/src/Pages/Records/index.tsx
+++ frontend/src/Pages/Records/index.tsx
@@ -1,8 +1,48 @@
 import React from 'react';
+import './styles.css';
 
 const Records = () => {
     return (
-        <div>Records</div>
+        <div className='page-container'>
+            <table className='records-table' cellPadding='0' cellSpacing='0'>
+                <thead>
+                    <tr>
+                        <th>INSTANTE</th>
+                        <th>NAME</th>
+                        <th>IDADE</th>
+                        <th>PLATAFORMA</th>
+                        <th>GÊNERO</th>
+                        <th>TITULO DO GAME</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>20/08/2022 12:56</td>
+                        <td>JONAS RIBEIRO</td>
+                        <td>30</td>
+                        <td>XBOX</td>
+                        <td>Ação - Aventura</td>
+                        <td>The last of US</td>
+                    </tr>
+                    <tr>
+                        <td>20/08/2022 12:56</td>
+                        <td>JONAS RIBEIRO</td>
+                        <td>30</td>
+                        <td>XBOX</td>
+                        <td>Ação - Aventura</td>
+                        <td>The last of US</td>
+                    </tr>
+                    <tr>
+                        <td>20/08/2022 12:56</td>
+                        <td>JONAS RIBEIRO</td>
+                        <td>30</td>
+                        <td>XBOX</td>
+                        <td>Ação - Aventura</td>
+                        <td>The last of US</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     )
 }
 
