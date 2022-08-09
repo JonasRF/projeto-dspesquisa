c56b9af0ff208d8b074183dd21179ffe845cae37
Jonas Ribeiro <jonasribeiro41@yahoo.com.br>
2022-08-09 11:44:55 -0300

    Update backend

--- backend/src/main/java/com/devsuperior/dspesquisa/repositories/RecordRepository.java
+++ backend/src/main/java/com/devsuperior/dspesquisa/repositories/RecordRepository.java
@@ -3,7 +3,7 @@ package com.devsuperior.dspesquisa.repositories;
 import java.time.Instant;
 
 import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -16,6 +16,6 @@ public interface RecordRepository extends JpaRepository<Record, Long> {
 	@Query("SELECT obj FROM Record obj WHERE "
 			+ "(coalesce(:min, null) IS NULL OR obj.moment >= :min) AND "
 			+ "(coalesce(:max, null) IS NULL OR obj.moment <= :max)")
-	Page<Record> findByMoments(Instant min, Instant max, Pageable pageable);
+	Page<Record> findByMoments(Instant min, Instant max, PageRequest pageRequest);
 
 }
